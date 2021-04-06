5b10f36ebce8ca39241845136f6a9dce278d8cf0
Divyansh <54841396+Divyansh4721@users.noreply.github.com>
2021-04-07 02:06:10 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var d = new Date();
-var t=document.getElementById("abc");
-setInterval(function(){
-  var d = new Date();
-  t.style.backgroundColor='#'+Math.floor(Math.random()*16777215).toString(16)
-  t.innerText=d.getHours()+':'+d.getMinutes()+':'+d.getSeconds();
-},1000)
